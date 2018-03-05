ccd01a0116b2be92ac977f64b2577a55b34f27c3
Ryan Scarff <williamscarff@Williams-MacBook-Pro.local>
2018-03-05 08:31:12 -0800

    functionality set up

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bailey');
+INSERT INTO cats (name, sleepy) VALUES ('Pickels', true);
+INSERT INTO cats (name, sleepy) VALUES ('Mr. Snuggles', true);
+INSERT INTO cats (name, sleepy) VALUES ('Reverse Farley', true);
+INSERT INTO cats (name) VALUES ('Lieutenant');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,56 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-sleep").on("click", function(event) {
+    var id = $(this).data("id");
+    var newSleep = 1;
+
+    var newSleepState = {
+      sleepy: newSleep
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/cats/" + id, {
+      type: "PUT",
+      data: newSleepState
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  // $(".delete").on("click", function(event) {
+  //   var id = $(this).data("id");
+
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "DELETE",
+  //     data: id
+  //   }).then(
+  //   function() {
+  //     console.log(id + " cat deleted")
+  //     location.reload();
+  //   })
+  // })
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newCat = {
+      name: $("#ca").val().trim(),
+      sleepy: 0
+    };
+
+    // Send the POST request.
+    $.ajax("/api/cats", {
+      type: "POST",
+      data: newCat
+    }).then(
+      function() {
+        console.log("created new cat");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
